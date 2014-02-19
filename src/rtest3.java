588d20813bd0c72045f3243d55c6709cdea62871
starqiu <674867812@qq.com>
2014-02-19 14:52:47 +0800

    test git in eclipse

---
+++ src/rtest3.java
@@ -0,0 +1,31 @@
+/*
+ * ============================================================
+ * The SSE USTC Software License
+ * 
+ * rtest3.java
+ * 2014-2-19
+ * 
+ * Copyright (c) 2006 China Payment and Remittance Service Co.,Ltd        
+ * All rights reserved.
+ * ============================================================
+ */
+
+/**
+ * 实现功能： 
+ * <p>
+ * date	        author            email		           notes<br />
+ * ----------------------------------------------------------------<br />
+ *2014-2-19  邱星            starqiu@mail.ustc.edu.cn	    新建类<br /></p>
+ *
+ */
+public class rtest3 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+	}
+
+}
+
